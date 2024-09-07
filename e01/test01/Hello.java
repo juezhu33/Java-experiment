41f8b5f7031ad2ce4d183503d90a5dc9152ccb77
Juzel <xjuzel@gmail.com>
2024-09-07 23:30:13 +0800

    2024_0907_2329

---
+++ e01/test01/Hello.java
@@ -0,0 +1,15 @@
+package experiment.e01.test01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好，欢迎学习Java");
+        A a = new A();
+        a.fA();
+    }
+}
+
+class A{
+    void fA() {
+        System.out.println("We are students");
+    }
+}
---
+++ e01/test02/B.java
@@ -0,0 +1,7 @@
+package experiment.e01.test02;
+
+public class B {
+    void fB() {
+        System.out.println("I am B");
+    }
+}
